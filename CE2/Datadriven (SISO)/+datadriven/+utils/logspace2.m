4c4a3f42af693ad458b287ca5cc751b68454a31d
Hugo Penichou <hugo.penichou@epfl.ch>
2024-05-07 21:04:15 +0200

    last file

--- CE2/Datadriven (SISO)/+datadriven/+utils/logspace2.m
+++
@@ -1,8 +0,0 @@
-function w = logspace2(a,b,c)
-% 
-%   ------------------------------------------------------------------------
-%   Copyright 2021 Philippe Schuchert, DDMAC, EPFL
-% 
-w = logspace(log10(a),log10(b),c);
-w(end) = b;
-end
